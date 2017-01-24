6f7caa30d515393498565fa03cb8fe7af4753774
kiki_kiswanto <kiki.kiswanto@hellobisnis.com>
2017-01-24 14:17:57 +0700

    add loarn routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,39 @@
 Rails.application.routes.draw do
+  get 'page/index'
+
+  get 'page/new'
+
+  get 'page/edit'
+
+  get 'articles/index'
+
+  get 'articles/new'
+
+  get 'articles/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => "page#index"
+  resources :news do
+  	member do
+
+      get 'special'
+
+      post 'discount'
+
+    end
+
+    collection do
+
+      get 'special1'
+
+      post 'discount1'
+
+    end
+  end
+  get 'bycycles/:id/buy' => 'bycycles#buy', as: :buy
+  namespace :api do
+
+    resources :post
+
+  end
 end
