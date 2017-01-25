d14588fd823ce0e5d15dd904c206e16854beeb21
kiki_kiswanto <kiki.kiswanto@hellobisnis.com>
2017-01-25 17:15:39 +0700

    add readmore

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,7 @@ class Article < ApplicationRecord
 	validates :title, presence: true,
 		length: { minimum: 5 }
 	validates :status, presence: true
+
+	default_scope {where(status: '1')}
+	scope :status_active, -> {where(status: '1')}
 end
